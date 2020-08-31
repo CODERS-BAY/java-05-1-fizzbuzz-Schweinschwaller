feb9ee5877245318a0a8f3003408538657c52bf8
Georg Schweinschwaller <georg.schweinschwaller@live.at>
2020-08-31 11:30:25 +0200

    changed remove the ()

--- src/Repetitions.java
+++ src/Repetitions.java
@@ -2,11 +2,11 @@ public class Repetitions {
     public static void main(String[] args) {
 
         for (int i = 1; i <= 100; i++) {
-            if ((i % 3 == 0) && (i % 5 == 0)) {
+            if (i % 3 == 0 && i % 5 == 0) {
                 System.out.print("FizzBuzz(" + i + ")! ");
-            } else if ((i % 3 == 0)) {
+            } else if (i % 3 == 0) {
                 System.out.print("Fizz(" + i + ")! ");
-            } else if ((i % 5 == 0)) {
+            } else if (i % 5 == 0) {
                 System.out.print("Buzz(" + i + ")! ");
             } else {
                 System.out.print(i + "! ");
